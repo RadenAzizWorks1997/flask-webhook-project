a08773c5c2b92ea9730c37548a2db190e337de6e
Raden Aziz <radenaziz.works@gmail.com>
2024-12-29 18:59:16 +0700

    Update proyek

--- app.py
+++ app.py
@@ -5,25 +5,32 @@ app = Flask(__name__)
 
 @app.route('/callback')
 def callback():
-    # Mendapatkan parameter 'code' dari URL
-    code = request.args.get('code')
+    try:
+        # Ambil parameter 'code' dari URL
+        code = request.args.get('code')
 
-    if code:
-        # Tukar code dengan token akses menggunakan API Instagram (atau Facebook)
+        # Jika 'code' tidak ada
+        if not code:
+            return jsonify(message="Code tidak ditemukan!"), 400
+
+        # Proses 'code' untuk mendapatkan token akses dari Instagram
         access_token_url = f"https://graph.instagram.com/access_token?client_id={YOUR_APP_ID}&client_secret={YOUR_APP_SECRET}&grant_type=authorization_code&redirect_uri={YOUR_REDIRECT_URI}&code={code}"
 
         # Mengirimkan permintaan untuk mendapatkan access token
         response = requests.get(access_token_url)
         data = response.json()
 
+        # Menyimpan access_token atau proses lebih lanjut
         if "access_token" in data:
-            # Menyimpan access_token atau proses lebih lanjut
             access_token = data["access_token"]
             return jsonify({"message": "Akses berhasil!", "access_token": access_token})
         else:
             return jsonify({"message": "Gagal mendapatkan access token!"}), 400
-    else:
-        return jsonify({"message": "Code tidak ditemukan!"}), 400
+
+    except Exception as e:
+        # Tangani jika terjadi error lainnya
+        return jsonify(message="Terjadi kesalahan pada server", error=str(e)), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
